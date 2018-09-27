9a4fd2cc7fc65ce1a769ab7f6d92e5cf2828362c
Sashka90 <opachki88@yandex.ru>
2018-09-27 18:00:26 +0300

    добавил неск функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,29 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        hello("world");
+
+
+        double len=6;
+        System.out.println("Площадь со стороной "+len + "= " + area(len));
+
+        double a= 4;
+        double b = 5;
+        System.out.println("Площадь прямоугольника со сторонами "+ a+"и "+b+"= " + area1(a,b));
+    }
+
+
+    public static void hello(String xz) {
+        System.out.println("Hello " + xz);
+    }
+
+
+    public static double area(double l) {
+        return l*l;
+    }
+
+
+    public static double area1(double a, double b){
+        return a*b;
     }
 }
